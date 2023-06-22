ccca23bb3e899d3b2f7a0c457bc46d1b2f1ed766
Mik Zibrov <33165240+zdarovzibrov@users.noreply.github.com>
2023-06-22 23:46:29 +0300

    branch level-3 fix. fonts zasechki, strkelka icon, semantika teg header-main-footer and other

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -4,13 +4,15 @@ import "./NotFound.css";
 
 function NotFound() {
   return (
-    <section className="pagenot-found">
-      <h2 className="pagenot-found__title">404</h2>
-      <p className="pagenot-found__subtitle">Страница не найдена.</p>
-      <Link to="/" className="pagenot-found__btn">
-        Назад
-      </Link>
-    </section>
+    <div className="container__main">
+      <section className="pagenot-found">
+        <h2 className="pagenot-found__title">404</h2>
+        <p className="pagenot-found__subtitle">Страница не найдена.</p>
+        <Link to="/" className="pagenot-found__btn">
+          Назад
+        </Link>
+      </section>
+    </div>
   );
 }
 
